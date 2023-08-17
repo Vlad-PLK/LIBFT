27e48aee70b073691bd801b92816decfbde60359
Vlad-PLK <leonpolo365@gmail.com>
2023-08-17 13:30:26 +0200

    comments

--- libft/lists/ft_lstsize.c
+++ libft/lists/ft_lstsize.c
@@ -6,11 +6,15 @@
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 19:41:43 by vpolojie          #+#    #+#             */
-/*   Updated: 2023/08/15 19:46:14 by vpolojie         ###   ########.fr       */
+/*   Updated: 2023/08/17 13:30:25 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+	ft_lstsize -> if lst exists, returns the size of it
+*/
 
 int	ft_lstsize(t_list *lst)
 {
